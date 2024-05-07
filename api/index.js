9133ecefcb84c4a222bcb14ad8e92d69a24894c1
sinilaa <emilia.laamanen@gmail.com>
2024-05-07 21:43:32 +0300

    strictquey change

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 // Serve static files
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', false);
 // Connect to MongoDB database using environment variable for URI
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
